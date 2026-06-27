89a736507ee32a318dc8e82446de15ebbf02c8d0
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-06-27 15:34:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
+
 import { Client, GatewayIntentBits } from "discord.js";
 import express from "express";
 import admin from "firebase-admin";
 
-/* 🔐 FIREBASE */
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
+/* ðŸ” FIREBASE */
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,59 +13,62 @@ admin.initializeApp({
 
 const db = admin.database();
 
-/* 🌐 WEB SERVER */
+/* ðŸŒ WEB SERVER */
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
-  console.log("🌐 Web activa en Render");
+  console.log("ðŸŒ Web activa en Render");
 });
 
-/* 🤖 DISCORD BOT */
+/* ðŸ¤– DISCORD BOT */
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageTyping
   ]
 });
 
 client.once("ready", () => {
-  console.log(`🤖 Logueado como ${client.user.tag}`);
+  console.log(`ðŸ¤– Logueado como ${client.user.tag}`);
 });
 
-/* 💬 DISCORD → FIREBASE */
+/* typing discord */
+client.on("typingStart", (typing) => {
+  db.ref("typing/discord").set({
+    username: typing.user?.username || "alguien",
+    time: Date.now()
+  });
+});
+
+/* ðŸ’¬ DISCORD â†’ FIREBASE */
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
   await db.ref("discordMessages").push({
     nickname: message.member?.displayName || message.author.username,
     username: message.author.username,
-    avatar: message.author.displayAvatarURL({
-      extension: "png",
-      size: 128
-    }),
+    avatar: message.author.displayAvatarURL({ size: 128 }),
 
     text: message.content,
     server: message.guild?.name || "DM",
     timestamp: Date.now()
   });
+
+  db.ref("typing/discord").remove();
 });
 
-/* 🌐 WEB → DISCORD */
+/* ðŸŒ WEB â†’ DISCORD */
 db.ref("webMessages").on("child_added", async (snap) => {
   const data = snap.val();
   if (!data?.text) return;
 
-  const channels = client.channels.cache;
-  const channel = channels.find(c => c.isTextBased());
-
-  if (channel) {
-    await channel.send(data.text);
-  }
+  const channel = client.channels.cache.find(c => c.isTextBased());
+  if (channel) await channel.send(data.text);
 });
 
 client.login(process.env.DISCORD_TOKEN);
-
